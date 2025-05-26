5d55e93dc3958fd60a67b5df3f7ea1ad9d94a900
Liam Adams <liam1540@googlemail.com>
2025-05-25 20:36:48 -0600

    Update inverse transform to support multiple arguments, and some additonaly primitives. Update example to test inverse transform.

--- jax_ext_inverse.py
+++ jax_ext_inverse.py
@@ -2,8 +2,9 @@ from functools import wraps
 
 import jax
 from jax import lax
-from jax._src import pjit
 from jax.extend import core
+from jax.tree_util import tree_flatten, tree_unflatten
+from jax._src import pjit
 from jax._src.util import safe_map
 import jax.numpy as jnp
 
@@ -25,8 +26,8 @@ def inverse(fun, *fwd_args):
   >>> from jax_ext_inverse import inverse
   >>> 
   >>> def g(src):
-  >>>   dst = jnp.flip(src, axis=0) # flip latitude axis
-  >>>   dst = jnp.swapaxes(dst, 0, 1) # (lat, lon)
+  >>>   dst = jnp.flip(src, axis=0)
+  >>>   dst = jnp.swapaxes(dst, 0, 1)
   >>>   dst = jnp.reshape(dst, (dst.shape[0] * dst.shape[1], 1))
   >>>   return dst
   >>> 
@@ -37,12 +38,42 @@ def inverse(fun, *fwd_args):
   >>> assert jnp.allclose(x, z)
   """
   @wraps(fun)
-  def wrapped(*args):
-    # Since we assume unary functions, we won't worry about flattening and
-    # unflattening arguments.
+  def wrapped(*inv_args):
+    
     closed_jaxpr = jax.make_jaxpr(fun)(*fwd_args)
-    out = inverse_jaxpr(closed_jaxpr.jaxpr, closed_jaxpr.literals, *args)
-    return out[0]
+    
+    # Get the pytree structures for the inverse function outputs
+    # i.e. the pytree structures for the forward function inputs
+    ret_counts = []
+    ret_trees = []
+    for fwd_arg in fwd_args:
+      fwd_flat_args, ret_tree = tree_flatten(fwd_arg)
+      ret_counts.append(len(fwd_flat_args))
+      ret_trees.append(ret_tree)
+
+    # Flatten each inverse function input and concatenate them into a single list
+    flat_args = []
+    arg_trees = []
+    for arg in inv_args:
+      flat_arg, arg_tree = tree_flatten(arg)
+      flat_args.extend(flat_arg)
+      arg_trees.append(arg_tree)
+    
+    # Evaluate the inverse function
+    inv_args = flat_args
+    out = inverse_jaxpr(closed_jaxpr.jaxpr, closed_jaxpr.literals, *inv_args)
+
+    # Unflatten the inverse function output
+    start_idx = 0
+    returns = []
+    for count, tree in zip(ret_counts, ret_trees):
+      ret = tree_unflatten(tree, out[start_idx:start_idx+count])
+      returns.append(ret)
+      start_idx += count
+
+    if len(returns) == 1:
+      return returns[0]
+    return tuple(returns)
   return wrapped
 
 def inverse_jaxpr(jaxpr, consts, *args):
@@ -55,6 +86,7 @@ def inverse_jaxpr(jaxpr, consts, *args):
 
   def write(var, val):
     env[var] = val
+
   # Args now correspond to Jaxpr outvars
   safe_map(write, jaxpr.outvars, args)
   safe_map(write, jaxpr.constvars, consts)
@@ -71,8 +103,12 @@ def inverse_jaxpr(jaxpr, consts, *args):
           f"{eqn.primitive} does not have registered inverse.")
 
     # Assuming a unary function
-    outval = inv_fnc(eqn, *invals)
-    safe_map(write, eqn.invars, [outval])
+    outval = inv_fnc(env, eqn, *invals)
+    if not isinstance(outval, list):
+      outval = [outval]
+    safe_map(write, eqn.invars, outval)
+    
+    # safe_map(write, eqn.invars, [outval])
   return safe_map(read, jaxpr.invars)
 
 
@@ -82,13 +118,20 @@ def eqn_params(eqn):
 def eqn_input_shapes(eqn):
   return [var.aval.shape for var in eqn.invars]
 
+def eqn_input_shape(eqn):
+  input_shapes = eqn_input_shapes(eqn)
+  assert len(input_shapes) == 1
+  return input_shapes[0]
 
-def inv_rev(eqn, val):
+def eqn_invars(eqn):
+  return eqn.invars
+
+
+def inv_rev(env, eqn, val):
   params = eqn_params(eqn)
-  dimensions = params['dimensions']
-  return lax.rev(val, dimensions)
+  return lax.rev(val, params['dimensions'])
 
-def inv_transpose(eqn, val):
+def inv_transpose(env, eqn, val):
   params = eqn_params(eqn)
   permutation = params['permutation']
   reversed_permutation = [0] * len(permutation)
@@ -96,20 +139,77 @@ def inv_transpose(eqn, val):
     reversed_permutation[v] = idx
   return jnp.transpose(val, reversed_permutation)
 
-def inv_reshape(eqn, val):
-  input_shapes = eqn_input_shapes(eqn)
-  assert len(input_shapes) == 1
-  return jnp.reshape(val, input_shapes[0])
+def inv_reshape(env, eqn, val):
+  input_shapes = eqn_input_shape(eqn)
+  params = eqn_params(eqn)
+  assert params['sharding'] is None
+  return jnp.reshape(val, input_shapes)
 
-def inv_pjit(eqn, *vals):
+def inv_pjit(env, eqn, *vals):
   params = eqn_params(eqn)
   closed_jaxpr = params['jaxpr']
   return inverse_jaxpr(closed_jaxpr.jaxpr, closed_jaxpr.literals, *vals)[0]
+
+def inv_slice(env, eqn, val):
+  params = eqn_params(eqn)
+  start_indices = params['start_indices']
+  limit_indices = params['limit_indices']
+  assert params['strides'] is None 
+  assert tuple(end - start for end, start in zip(limit_indices, start_indices)) == eqn.outvars[0].aval.shape
+  outvar = eqn.invars[0]
+  if outvar not in env:
+    outval = jnp.zeros(outvar.aval.shape, dtype=outvar.aval.dtype)
+    def write(var, val):
+      env[var] = val
+    safe_map(write, eqn.invars, [outval])
+  outval = env[outvar]
+  return lax.dynamic_update_slice(outval, val, start_indices)
+
+def inv_squeeze(env, eqn, val):
+  params = eqn_params(eqn)
+  return jnp.expand_dims(val, params['dimensions'])
+
+def inv_concatenate(env, eqn, val):
+  input_shapes = eqn_input_shapes(eqn)
+  params = eqn_params(eqn)
+  return jnp.split(val, len(input_shapes), axis=params['dimension'])
+
+def inv_broadcast_in_dim(env, eqn, val):
+  input_shape = eqn_input_shape(eqn)
+  params = eqn_params(eqn)
+  broadcast_dimensions = params['broadcast_dimensions']
+  shape = params['shape']
+  assert params['sharding'] is None
+
+  start_indices = [0] * len(shape)
+  limit_indices = [1] * len(shape)
+  for idx, dim in enumerate(broadcast_dimensions):
+    limit_indices[dim] = input_shape[idx]
+  
+  squeeze_dimensions = []
+  for dim in range(len(shape)):
+    if dim not in broadcast_dimensions:
+      squeeze_dimensions.append(dim)
+  squeeze_dimensions = tuple(squeeze_dimensions)
+
+  return lax.squeeze(
+    lax.slice(val, start_indices, limit_indices, None),
+    dimensions=squeeze_dimensions
+  )
+
+def inv_split(env, eqn, *vals):
+  params = eqn_params(eqn)
+  return lax.concatenate(vals, dimension=params['axis'])
+
 
 inverse_registry = {}
 inverse_registry[lax.reshape_p] = inv_reshape
 inverse_registry[lax.transpose_p] = inv_transpose
 inverse_registry[lax.rev_p] = inv_rev
 inverse_registry[pjit.pjit_p] = inv_pjit
-
+inverse_registry[lax.slice_p] = inv_slice
+inverse_registry[lax.squeeze_p] = inv_squeeze
+inverse_registry[lax.concatenate_p] = inv_concatenate
+inverse_registry[lax.broadcast_in_dim_p] = inv_broadcast_in_dim
+inverse_registry[lax.split_p] = inv_split
 
--- main.py
+++ main.py
@@ -1,18 +1,104 @@
+from functools import partial
+
 import jax.numpy as jnp
 from jax_ext_inverse import inverse
 
-def g(src):
-  dst = jnp.flip(src, axis=0) # flip latitude axis
-  dst = jnp.swapaxes(dst, 0, 1) # (lat, lon)
-  dst = jnp.reshape(dst, (dst.shape[0] * dst.shape[1], 1))
-  return dst
+def test_array():
 
-x = jnp.array([[1, 2, 3], [4, 5, 6]])
-y = g(x)
-g_inv = inverse(g, x)
-z = g_inv(y)
+  def f(src):
+    dst = jnp.flip(src, axis=0)
+    dst = jnp.swapaxes(dst, 0, 1)
+    dst = jnp.reshape(dst, (dst.shape[0] * dst.shape[1], 1))
+    return dst
 
-print(f"Original x:\n{x}")
-print(f"Transformed y:\n{y}")
-print(f"Inverse z:\n{z}")
-print(f"Inverse z equals original x: {jnp.allclose(x, z)}")
+  x = jnp.array([[1, 2, 3], [4, 5, 6]])
+  y = f(x)
+
+  f_inv = inverse(f, x)
+  
+  z = f_inv(y)
+
+  # print(f"Original x:\n{x}")
+  # print(f"Transformed y:\n{y}")
+  # print(f"Inverse z:\n{z}")
+
+  assert jnp.allclose(x, z)
+
+def test_dict():
+
+  ctx = {"x1": ["y1", "y2"], "x2": ["y3"]}
+  def f_ctx(ctx, src):
+    dst = {}
+    for k, v in src.items():
+      new_ks = ctx[k]
+      for idx, new_k in enumerate(new_ks):
+        dst[new_k] = v[idx]
+    return dst
+  f = partial(f_ctx, ctx)
+
+  x = {"x1": jnp.array([[1,2,3],[4,5,6]]), "x2": jnp.array([[7,8,9],])}
+  y = f(x)
+
+  f_inv = inverse(f, x)
+
+  z = f_inv(y)
+
+  # print(f"Original x:\n{x}")
+  # print(f"Transformed y:\n{y}")
+  # print(f"Inverse z:\n{z}")
+
+  assert x.keys() == z.keys()
+  for k in x.keys():
+    assert jnp.allclose(x[k], z[k])
+
+
+def test_multiple_args():
+
+  def f(src1, src2, src3):
+    return jnp.stack([src1, src2, src3], axis=1)
+
+  x1 = jnp.array([[1, 2, 3], [4, 5, 6]])
+  x2 = jnp.array([[7, 8, 9], [10, 11, 12]])
+  x3 = jnp.array([[13, 14, 15], [16, 17, 18]])
+  y = f(x1, x2, x3)
+  
+  f_inv = inverse(f, x1, x2, x3)
+  
+  z1, z2, z3 = f_inv(y)
+  
+  # print(f"Original x1:\n{x1}")
+  # print(f"Original x2:\n{x2}")
+  # print(f"Transformed y:\n{y}")
+  # print(f"Inverse z1:\n{z1}")
+  # print(f"Inverse z2:\n{z2}")
+
+  assert jnp.allclose(x1, z1)
+  assert jnp.allclose(x2, z2)
+  assert jnp.allclose(x3, z3)
+
+
+def test_multiple_rets():
+
+  def f(x):
+    return jnp.split(x, 2, axis=0)
+
+  x = jnp.array([[1, 2], [3, 4]])
+  y1, y2 = f(x)
+  
+  f_inv = inverse(f, x)
+  
+  z = f_inv(y1, y2)
+  
+  # print(f"Original x:\n{x}")
+  # print(f"Transformed y1:\n{y1}")
+  # print(f"Transformed y2:\n{y2}")
+  # print(f"Inverse z:\n{z}")
+
+  assert jnp.allclose(x, z)
+
+
+if __name__ == "__main__":
+  test_array()
+  test_dict()
+  test_multiple_args()
+  test_multiple_rets()
